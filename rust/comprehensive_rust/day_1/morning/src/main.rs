ad4482c785a40b08ad7d9aa7af89506dc216e224
Kostya Bazhanov <yazevnul@users.noreply.github.com>
2024-07-20 17:03:36 +0100

    solution for day 1 morning exercise

---
+++ rust/comprehensive_rust/day_1/morning/src/main.rs
@@ -0,0 +1,12 @@
+fn fib(n: u32) -> u32 {
+    if n < 2 {
+        n
+    } else {
+        fib(n - 2) + fib(n - 1)
+    }
+}
+
+fn main() {
+    let n = 20;
+    println!("fib({n}) = {}", fib(n));
+}
