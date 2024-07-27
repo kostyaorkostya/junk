8cf01f89a6867fabda3ad60903815f6a10192d2a
Kostya Bazhanov <yazevnul@users.noreply.github.com>
2024-07-27 15:17:20 +0100

    generic (?) min

---
+++ rust/comprehensive_rust/day_2/afternoon/generic_min/src/main.rs
@@ -0,0 +1,21 @@
+fn min<T>(left: T, right: T) -> T
+where
+    T: PartialOrd<T>,
+{
+    if left <= right {
+        left
+    } else {
+        right
+    }
+}
+
+fn main() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+
+    assert_eq!(min('a', 'z'), 'a');
+    assert_eq!(min('7', '1'), '1');
+
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
