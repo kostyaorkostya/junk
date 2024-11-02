51369a3673b3d8ef608f24eb515d5926c2e209e2
Kostya <happy.boat9793@fastmail.com>
2024-11-02 11:24:52 +0000

    works

---
+++ rust/comprehensive_rust/day_4/morning/method_chaining/src/main.rs
@@ -0,0 +1,41 @@
+/// Calculate the differences between elements of `values` offset by `offset`,
+/// wrapping around from the end of `values` to the beginning.
+///
+/// Element `n` of the result is `values[(n+offset)%len] - values[n]`.
+#[allow(dead_code)]
+fn offset_differences(offset: usize, values: Vec<i32>) -> Vec<i32> {
+    if values.is_empty() {
+        Vec::default()
+    } else {
+        std::iter::zip(values.iter(), values.iter().cycle().skip(offset))
+            .map(|(x, y)| y - x)
+            .collect::<Vec<_>>()
+    }
+}
+
+#[test]
+fn test_offset_one() {
+    assert_eq!(offset_differences(1, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+    assert_eq!(offset_differences(1, vec![1, 3, 5]), vec![2, 2, -4]);
+    assert_eq!(offset_differences(1, vec![1, 3]), vec![2, -2]);
+}
+
+#[test]
+fn test_larger_offsets() {
+    assert_eq!(offset_differences(2, vec![1, 3, 5, 7]), vec![4, 4, -4, -4]);
+    assert_eq!(offset_differences(3, vec![1, 3, 5, 7]), vec![6, -2, -2, -2]);
+    assert_eq!(offset_differences(4, vec![1, 3, 5, 7]), vec![0, 0, 0, 0]);
+    assert_eq!(offset_differences(5, vec![1, 3, 5, 7]), vec![2, 2, 2, -6]);
+}
+
+#[test]
+fn test_degenerate_cases() {
+    assert_eq!(offset_differences(1, vec![0]), vec![0]);
+    assert_eq!(offset_differences(1, vec![1]), vec![0]);
+    let empty: Vec<i32> = vec![];
+    assert_eq!(offset_differences(1, empty), vec![]);
+}
+
+fn main() {
+    println!("Hello, world!");
+}
