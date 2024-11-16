225ba1fb9c0cf7e2921c777bb593abe1a6c98f11
Kostya <happy.boat9793@fastmail.com>
2024-11-16 17:34:07 +0000

    more fixes

--- rust/comprehensive_rust/day_4/morning/gui-modules/src/main.rs
+++ rust/comprehensive_rust/day_4/morning/gui-modules/src/main.rs
@@ -9,5 +9,5 @@ fn main() {
     let mut window = Window::new("Rust GUI Demo 1.23");
     window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
     window.add_widget(Box::new(Button::new("Click me!")));
-    window.draw();
+    window.draw().unwrap();
 }
