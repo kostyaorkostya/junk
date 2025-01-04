c9e1c42842bfa7ceab7bf770ccf670ce3aeb8aa5
Kostya <happy.boat9793@fastmail.com>
2025-01-04 15:19:47 +0000

    better

--- rust/tonic/examples/helloworld-tonic/src/server.rs
+++ rust/tonic/examples/helloworld-tonic/src/server.rs
@@ -1,5 +1,5 @@
 use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use hello_world::{HelloRequest, HelloResponse};
 use tonic::{transport::Server, Request, Response, Status};
 
 pub mod hello_world {
@@ -11,20 +11,23 @@ pub struct MyGreeter {}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
-    async fn say_hello(&self, request: Request<HelloRequest>) -> Response<HelloReply, Status> {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = HelloReply {
-            message: format!("Hello {}", request.into_innter().name),
+        let resp = HelloResponse {
+            message: format!("Hello {}", request.into_inner().name),
         };
 
-        Ok(Response::new(reply))
+        Ok(Response::new(resp))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse?;
+    let addr = "[::1]:50051".parse()?;
     let greeter = MyGreeter::default();
 
     Server::builder()
